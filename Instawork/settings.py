3ffe2e471ddab213d08328b41d3607cbe9fa94d9
eugenielm <eugenie.le.moulec@gmail.com>
2019-06-28 16:17:22 -0700

    Create 'instateam' app to manage team members

--- Instawork/settings.py
+++ Instawork/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instateam.apps.InstateamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ instateam/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InstateamConfig(AppConfig):
+    name = 'instateam'
