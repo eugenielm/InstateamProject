93aa865647376a3a364222cb4a31e2514d97b11a
eugenielm <eugenie.le.moulec@gmail.com>
2019-06-30 14:53:52 -0700

    Remove code duplication

--- instateam/views.py
+++ instateam/views.py
@@ -6,6 +6,19 @@ from django.views.generic import ListView, CreateView, UpdateView
 from .models import TeamMember
 
 
+def update_form(form):
+    "Set the form's placeholders and radio checkboxes labels"
+    form.fields['first_name'].widget.attrs['placeholder'] = 'first name'
+    form.fields['last_name'].widget.attrs['placeholder'] = 'last name'
+    form.fields['phone'].widget.attrs['placeholder'] = 'phone number - e.g. 111-222-3333'
+    form.fields['email'].widget.attrs['placeholder'] = 'email'
+    form.fields['role'].widget = forms.RadioSelect(choices=[
+        ('regular', "Regular - Can't delete members"), 
+        ('admin',  'Admin - Can delete members')
+    ])
+    return form
+
+
 def index(request):
     return redirect('team_members_list')
 
@@ -22,20 +35,12 @@ class TeamMembersCreate(CreateView):
     fields = ['first_name', 'last_name', 'email', 'phone', 'role']
     template_name = 'instateam/teammembers_cud.html'
 
-    def get_success_url(self):
-        return reverse('team_members_list')
-
     def get_form(self, *args):
         form = super().get_form(*args)
-        form.fields['first_name'].widget.attrs['placeholder'] = 'first name'
-        form.fields['last_name'].widget.attrs['placeholder'] = 'last name'
-        form.fields['phone'].widget.attrs['placeholder'] = 'phone number - e.g. 111-222-3333'
-        form.fields['email'].widget.attrs['placeholder'] = 'email'
-        form.fields['role'].widget = forms.RadioSelect(choices=[
-            ('regular', "Regular - Can't delete members"), 
-            ('admin',  'Admin - Can delete members')
-        ])
-        return form
+        return update_form(form)
+
+    def get_success_url(self):
+        return reverse('team_members_list')
 
 
 class TeamMembersUpdate(UpdateView):
@@ -44,20 +49,12 @@ class TeamMembersUpdate(UpdateView):
     template_name = 'instateam/teammembers_cud.html'
     context_object_name = 'teammember'
 
-    def get_success_url(self):
-        return reverse('team_members_list')
-    
     def get_form(self, *args):
         form = super().get_form(*args)
-        form.fields['first_name'].widget.attrs['placeholder'] = 'first name'
-        form.fields['last_name'].widget.attrs['placeholder'] = 'last name'
-        form.fields['phone'].widget.attrs['placeholder'] = 'phone number - e.g. 111-222-3333'
-        form.fields['email'].widget.attrs['placeholder'] = 'email'
-        form.fields['role'].widget = forms.RadioSelect(choices=[
-            ('regular', "Regular - Can't delete members"), 
-            ('admin',  'Admin - Can delete members')
-        ])
-        return form
+        return update_form(form)
+
+    def get_success_url(self):
+        return reverse('team_members_list')
 
 
 def delete_teammember(request, pk):
