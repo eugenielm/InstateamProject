b79e955ba813ffcea77f75cef4f864f41dffc17e
eugenielm <eugenie.le.moulec@gmail.com>
2019-07-03 14:22:32 -0700

    Use model validator for TeamMember's first and last name fields instead of overriding clean_fields()

--- instateam/models.py
+++ instateam/models.py
@@ -1,15 +1,20 @@
 import re
 
-from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 import phonenumbers
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class TeamMember(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
+    first_name = models.CharField(max_length=60, validators=[RegexValidator(
+        regex="^[a-zA-Z\-\s']{1,60}$", 
+        message="A first name can contain only letters, -, ' and white spaces.")])
+    last_name = models.CharField(max_length=60, validators=[RegexValidator(
+        regex="^[a-zA-Z\-\s']{1,60}$", 
+        message="A last name can contain only letters, -, ' and white spaces.")])
     # A CharField that uses an EmailValidator for checking (max_length=254)
     email = models.EmailField(unique=True)
     # https://github.com/stefanfoulis/django-phonenumber-field
@@ -36,22 +41,3 @@ class TeamMember(models.Model):
     
     def formatted_phone_e164(self):
         return phonenumbers.format_number(self.phone, phonenumbers.PhoneNumberFormat.E164)
-
-    def clean_fields(self, *args, **kwargs):
-        # takes care of the email field validation and checks the presence of 
-        # first/last name, phone, and role
-        super().clean_fields(*args, **kwargs)
-        errors = {}
-        # make sure the first name is valid
-        if not re.match("^[a-zA-Z\-\s']{1,60}$", getattr(self, 'first_name')):
-            errors['first_name'] = "Please provide a valid first name."
-        # make sure the last name is valid
-        if not re.match("^[a-zA-Z\-\s']{1,60}$", getattr(self, 'last_name')):
-            errors['last_name'] = "Please provide a valid last name."
-        ### To be improved (?): first and last names should be allowed to contain accented characters
-        
-        if errors:
-            raise ValidationError({
-                k: ValidationError(_(v), code='invalid')\
-                    for k,v in errors.items()
-            })
